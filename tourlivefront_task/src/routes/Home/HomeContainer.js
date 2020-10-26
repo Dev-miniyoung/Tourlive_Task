1a0820c54d43969ff0cf723a37c1ffa545fcb5ef
YoungMin Lee <miniyoung37@gmail.com>
2020-10-27 02:08:55 +0900

    update home component

--- tourlivefront_task/src/routes/Home/HomeContainer.js
+++ tourlivefront_task/src/routes/Home/HomeContainer.js
@@ -1,11 +1,55 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import * as api from "../../api";
 
 class HomeContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initData: [],
+      resultData: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const response = await api.apiList.getTour();
+    const {
+      data: {
+        data: { results },
+      },
+    } = response;
+    if (response.status === 200) {
+      this.setState({
+        initData: results,
+      });
+    }
+  };
+
+  handleSearch = async (searchTerm) => {
+    const response = await api.apiList.getSerchTour(searchTerm);
+    console.log("response", response);
+    const {
+      data: {
+        data: { results },
+      },
+    } = response;
+    if (response.status === 200) {
+      this.setState({
+        resultData: results,
+      });
+    }
+  };
   render() {
+    const { initData, resultData } = this.state;
+    const { handleSearch } = this;
+
     return (
       <>
-        <HomePresenter />
+        <HomePresenter
+          initData={initData}
+          resultData={resultData}
+          handleSearch={handleSearch}
+        />
       </>
     );
   }
--- tourlivefront_task/src/routes/Home/HomePresenter.js
+++ tourlivefront_task/src/routes/Home/HomePresenter.js
@@ -1,8 +1,51 @@
 import React from "react";
 
-const HomePresenter = () => (
+import Card from "../../component/Card";
+import styled from "styled-components";
+import { Layout, Input } from "antd";
+
+const { Content } = Layout;
+const { Search } = Input;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+const ResultContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 40px;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const HomePresenter = ({ initData, resultData, handleSearch }) => (
   <>
-    <div>HomePresenter</div>
+    <Content style={{ margin: "0 auto", width: 850 }}>
+      <ButtonContainer>
+        <ResultContainer>
+          {resultData.length !== 0
+            ? `[ 검색완료 : ${resultData.length} 건 ]`
+            : null}
+        </ResultContainer>
+        <Search
+          placeholder="투어 검색"
+          style={{ width: 400, marginRight: 200 }}
+          onSearch={(e) => handleSearch(e)}
+          enterButton
+        />
+      </ButtonContainer>
+
+      {resultData.length !== 0 ? (
+        <Card data={resultData} />
+      ) : (
+        <Card data={initData} />
+      )}
+    </Content>
   </>
 );
 
