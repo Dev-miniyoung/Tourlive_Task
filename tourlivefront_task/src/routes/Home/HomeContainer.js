609d1a792a5c34528d9f0693d72d77ff2208e442
YoungMin Lee <miniyoung37@gmail.com>
2020-10-26 21:25:30 +0900

    add home component

---
+++ tourlivefront_task/src/routes/Home/HomeContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+class HomeContainer extends React.Component {
+  render() {
+    return (
+      <>
+        <HomePresenter />
+      </>
+    );
+  }
+}
+
+export default HomeContainer;
---
+++ tourlivefront_task/src/routes/Home/HomePresenter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomePresenter = () => (
+  <>
+    <div>HomePresenter</div>
+  </>
+);
+
+export default HomePresenter;
