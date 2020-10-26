20005b3f809e573bd60833bf5683ebf233826bc6
YoungMin Lee <miniyoung37@gmail.com>
2020-10-26 22:09:16 +0900

    add router component

---
+++ tourlivefront_task/src/component/Router.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { Layout } from "antd";
+
+import Header from "./Header";
+
+import HomeScreen from "../routes/Home";
+import DetailScreen from "../routes/Detail";
+
+export default () => (
+  <Router>
+    <>
+      <Layout className="layout" style={{ minHeight: "100vh" }}>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/detail/:id" exact component={DetailScreen} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Layout>
+    </>
+  </Router>
+);
